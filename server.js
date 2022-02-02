9365fd866ec83c3ee13fc8798339c08caba9ec8a
MissingNewt <chelby.sallady@gmail.com>
2022-02-01 21:47:28 -0800

    new method

--- server.js
+++ server.js
@@ -2,32 +2,30 @@ const path = require("path");
 const express = require("express");
 const session = require("express-session");
 const exphbs = require("express-handlebars");
+const routes = require("./controllers");
+const helpers = require("./utils/helpers");
+
+const sequelize = require("./config/connection");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const sequelize = require("./config/connection.js");
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const hbs = exphbs.create({ helpers });
 
 const sess = {
-  secret: "something secret",
+  secret: "Something secret",
   cookie: {},
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
-    db: sequelize
-  })
+    db: sequelize,
+  }),
 };
 
 app.use(session(sess));
 
-const hbs = exphbs.create({
-  helpers: {
-    format_date: date => {
-      return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
-    }
-  }
-});
 
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
@@ -36,9 +34,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use(require('./controllers/'));
+app.use(routes);
 
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
-  sequelize.sync({ force: false });
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 });
